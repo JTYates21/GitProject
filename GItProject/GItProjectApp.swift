65d76015e9ad366bcec9a8515206be441c99a061
JTYates21 <coolman232@gmail.com>
2022-04-18 09:47:59 -0600

    initial commit

---
+++ GItProject/GItProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GItProjectApp.swift
+//  GItProject
+//
+//  Created by Jacek Yates on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct GItProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
